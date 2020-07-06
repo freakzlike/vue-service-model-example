721de37cca010c6f0d8f0e88e6c85744b0995d39
freakzlike <freakzlike@gmail.com>
2020-07-06 20:16:50 +0200

    Add example of standalone field instance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
+    <hr/>
+
+    <div class="user-example-link">
+      See a complete service example
+      <router-link :to="{name: 'UserList'}">here.</router-link>
+    </div>
+
+    <hr/>
+
+    <standalone-field-example/>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      'standalone-field-example': () => import('./StandaloneFieldExample')
+    }
   }
 </script>
+
+<style scoped>
+  .user-example-link {
+    margin: 24px 0;
+  }
+</style>
---
+++ src/views/StandaloneFieldExample.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Standalone field example</h2>
+
+    <h5>Enter your Name:</h5>
+
+    <div>
+      <input-field :field="welcomeField"/>
+    </div>
+
+    <div>
+      <display-field :field="welcomeField"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {CharField, InputField, DisplayField} from 'vue-service-model'
+
+  class WelcomeMessageField extends CharField {
+    async valueFormatter () {
+      const name = await super.valueFormatter()
+      return name ? `Welcome ${name}!` : null
+    }
+  }
+
+  export default {
+    name: 'StandaloneFieldExample',
+    components: {InputField, DisplayField},
+    data: () => ({
+      welcomeField: new WelcomeMessageField({}, {value: null})
+    })
+  }
+</script>
